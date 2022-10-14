ed0f71eacfed95e9d9f920f4b67989ba4a994aea
WaterGame2023 <spam4alaric@gmail.com>
2022-10-14 01:18:54 -0500

    Push before the nuke

--- Python/python_detect.py
+++ Python/python_detect.py
@@ -3,9 +3,8 @@ import numpy as np
 import apriltag
 from networktables import NetworkTables
 
-#NetworkTables.initialize(server='10.25.31.2')
-#table.putXXX(name, value)
-#table.getXXX(name, default)
+#NetworkTables.initialize(server='10.25.31.2') #Uncomment when there is a NT server
+NT = NetworkTables.getTable("ceaseless-watcher")
 
 LINE_LENGTH = 5
 CENTER_COLOR = (0, 255, 0)
@@ -31,7 +30,7 @@ def plotText(image, center, color, text):
                        1, color, 3)
 
 detector = apriltag.Detector()
-cam = cv2.VideoCapture(2)
+cam = cv2.VideoCapture(0)
 
 #Resolution and frame rate settings
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
@@ -46,16 +45,24 @@ while looping:
 	# look for tags
     detections = detector.detect(grayimg)
     if not detections:
+        NT.putString("tagfound", 0)
         print("No Tag found")
     else:
         for detect in detections:
             print("tag_id: %s, center: %s" % (detect.tag_id, detect.center))
+
             image = plotPoint(image, detect.center, CENTER_COLOR)
             image = plotText(image, detect.center, CENTER_COLOR, detect.tag_id)
+
+            NT.putString("tag_center", detect.center)
+            NT.putString("tag_id", detect.tag_id)
+            NT.putString("tagfound", 1)
+
             for corner in detect.corners:
                 image = plotPoint(image, corner, CORNER_COLOR)
-    cv2.imshow('Result', image)
-    
+
+    cv2.imshow('Result', image) #Comment out when running in headless mode to not piss off python
+
     key = cv2.waitKey(100)
     if key == 13:
         looping = False
